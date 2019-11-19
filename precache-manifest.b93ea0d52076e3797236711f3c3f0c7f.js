96cfddbee4de6b2b16c2877c8069926f5eae5e13
Jordan Jensen <jordan@dotnomad.com>
2019-11-18 21:01:26 -0800

    Updates

---
+++ precache-manifest.b93ea0d52076e3797236711f3c3f0c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5855f8253057c5ccd6dae3e57cc608bc",
+    "url": "/ignota-dice-roller/index.html"
+  },
+  {
+    "revision": "44fc570083f154333e9f",
+    "url": "/ignota-dice-roller/static/css/main.9cd3ff85.chunk.css"
+  },
+  {
+    "revision": "986aad563dff9aa88fd8",
+    "url": "/ignota-dice-roller/static/js/2.e075a881.chunk.js"
+  },
+  {
+    "revision": "44fc570083f154333e9f",
+    "url": "/ignota-dice-roller/static/js/main.d8be0bc5.chunk.js"
+  },
+  {
+    "revision": "9c8a25f62ce6ccd98acf",
+    "url": "/ignota-dice-roller/static/js/runtime-main.4ff4a928.js"
+  }
+]);
